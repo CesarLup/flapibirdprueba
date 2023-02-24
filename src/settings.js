af8b848c0c24711ad3284c1d7cb052cc86e9630d
CesarLup <85419071+CesarLup@users.noreply.github.com>
2023-02-24 10:05:41 -0500

    Delete src directory

--- src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/xcesne.fire",orientation:"",jsList:[]};
